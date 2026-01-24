e4034526b1e4046b8c94e6f49d1fdd6f25e8d01e
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-01-24 18:19:22 +0200

    Format code with tabs and trailing commas

--- src/schemas/common.ts
+++ src/schemas/common.ts
@@ -3,44 +3,53 @@ import * as Schema from "effect/Schema";
 /**
  * Base error for all network-related failures
  */
-export class NetworkError extends Schema.TaggedError<NetworkError>()("NetworkError", {
-  message: Schema.String,
-  endpoint: Schema.String,
-  cause: Schema.Unknown
-}) {}
+export class NetworkError extends Schema.TaggedError<NetworkError>()(
+	"NetworkError",
+	{
+		message: Schema.String,
+		endpoint: Schema.String,
+		cause: Schema.Unknown,
+	},
+) {}
 
 /**
  * Base error for all API-related failures
  */
 export class ApiError extends Schema.TaggedError<ApiError>()("ApiError", {
-  code: Schema.String,
-  message: Schema.String,
-  endpoint: Schema.String
+	code: Schema.String,
+	message: Schema.String,
+	endpoint: Schema.String,
 }) {}
 
 /**
  * Error for failed schema validation
  */
-export class ValidationError extends Schema.TaggedError<ValidationError>()("ValidationError", {
-  message: Schema.String,
-  field: Schema.String,
-  received: Schema.Unknown
-}) {}
+export class ValidationError extends Schema.TaggedError<ValidationError>()(
+	"ValidationError",
+	{
+		message: Schema.String,
+		field: Schema.String,
+		received: Schema.Unknown,
+	},
+) {}
 
 /**
  * Error for configuration failures
  */
-export class ConfigError extends Schema.TaggedError<ConfigError>()("ConfigError", {
-  message: Schema.String,
-  key: Schema.String
-}) {}
+export class ConfigError extends Schema.TaggedError<ConfigError>()(
+	"ConfigError",
+	{
+		message: Schema.String,
+		key: Schema.String,
+	},
+) {}
 
 /**
  * Standard API error response format
  */
 export const ApiErrorResponseSchema = Schema.Struct({
-  code: Schema.String,
-  message: Schema.String
+	code: Schema.String,
+	message: Schema.String,
 });
 
 export type ApiErrorResponse = typeof ApiErrorResponseSchema.Type;
