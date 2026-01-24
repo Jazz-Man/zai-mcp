a3a05144f4f5a85061b3899afef5a0b6b434bb3d
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-01-24 18:40:47 +0200

    Use ZaiHttpClient for web reader tool

--- src/toolkit.ts
+++ src/toolkit.ts
@@ -1,13 +1,16 @@
 import { type Tool, Toolkit } from "@effect/ai";
 import { Effect, Layer } from "effect";
-import { WebReaderTool, WebReaderHandler } from "./tools/web-reader";
 import { ZaiHttpClient } from "./services/http-client";
+import { WebReaderTool } from "./tools/web-reader";
 
 export const ZaiToolkit = Toolkit.make(WebReaderTool);
 
 export const ZaiToolkitHandlers = ZaiToolkit.toLayer(
-	Effect.succeed({
-		webReader: WebReaderHandler,
+	Effect.gen(function* () {
+		const client = yield* ZaiHttpClient;
+		return {
+			webReader: (params) => client.readUrl(params),
+		};
 	}),
 ).pipe(Layer.provide(ZaiHttpClient.Default));
 
